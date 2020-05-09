155ae757725d4db2ba4245a44b3128fc972a7c91
Stacey Tannehill <staceytannehill@Staceys-MBP.attlocal.net>
2020-05-09 10:34:06 -0500

    commit

--- Source/JavaSC/app.js
+++ Source/JavaSC/app.js
@@ -1,1 +1,49 @@
 console.log('Hello World!');
+let me={
+    firstName: 'Stacey',
+    lastName: 'Tannehill'
+};
+
+const nifty= 50;
+
+//adding 5 and 4 to equal 9 here
+console.log(add(5, 4));
+function add(num1, num2) {
+    return num1 + num2;
+
+}
+
+alert("Hello World!"); 
+console.log('Hello World');    
+
+i=name
+var person=[ 
+{name:'Charles', age: 21},
+{name:'Abby', age:27},
+{name:"James", age: 18},
+{name:"John", age: 17}  
+];
+
+function checkAge([age, name]){
+    if (age > 21){
+        alert ("Sorry" + name +", you aren't old enought to view this page!");
+    }
+}
+
+for(var i = 0; i <= 4; ++i) {
+    console.log(i)
+}
+
+//Veggie time
+
+const veggies = ['Yuca', 'Kale', 'Argugula', 'Radish'];
+    let info= 'My favorite veggies are';
+    const para = document.querySelector('p');
+
+for (let i = 0; i < info.length; i++) {
+    info += veggies[i] + ",";
+}
+
+para.textContent = info;
+
+break;
