9f41f2a2bbd351cb37081cabb9835affff89bb46
staceylaurel <64053039+staceylaurel@users.noreply.github.com>
2020-05-10 22:12:32 -0500

    commit

--- Source/JavaSC/app.js
+++ Source/JavaSC/app.js
@@ -1,71 +1,98 @@
-console.log('Hello World!');
-let me={
+//4.
+let me = {
     firstName: 'Stacey',
     lastName: 'Tannehill'
 };
 
-const nifty= 50;
+//5.
+const nifty = 50;
+console.log(nifty);
 
-//adding 5 and 4 to equal 9 here
+//6. adding 5 and 4 to equal 9 here
 console.log(add(5, 4));
 function add(num1, num2) {
     return num1 + num2;
 
 }
+//7. NOTES!
 
-alert("Hello World!"); 
-console.log('Hello World');    
+//8.
+alert("Hello World!");
+console.log('Hello World');
 
-i=name
-var person=[ 
-{name:'Charles', age: 21},
-{name:'Abby', age:27},
-{name:"James", age: 18},
-{name:"John", age: 17}  
-];
+//9.
+function checkAge([age, name]) {
+    if (age < 21) {
+        alert("Sorry" + name + ", you aren't old enought to view this page!");
 
-function checkAge([age, name]){
-    if (age > 21){
-        alert ("Sorry" + name +", you aren't old enought to view this page!");
     }
-}
+    let charles = {
+        name: "Charles",
+        age: 21,
+    };
 
-for(var i = 0; i <= 4; ++i) {
+    let abby = {
+        name: 'Abby',
+        age: 27,
+    };
+
+    let james = {
+        name: "James",
+        age: 18,
+    };
+
+    let john = {
+        name: "John",
+        age: 17,
+    };
+}
+var times = 4;
+for (var i = 0; i < times; ++i) {
     console.log(i)
 }
 
-//Veggie time
-
-const veggies = ['Yuca', 'Kale', 'Argugula', 'Radish'];
-    let info= 'My favorite veggies are';
-    const para = document.querySelector('p');
-
-for (let i = 0; i < info.length; i++) {
-    info += veggies[i] + ",";
+//10. Veggie time
+var veggies = ["Kale", "Arugala", "Raddish"]
+for (var i = 0; i < veggies.length; i++) {
+    console.log(veggies[i]);
 }
 
-para.textContent = info;
-
-//11. Pets
-console.log(add[CockerDoodle[ + Barkley]]);
-
-var animal=[
-    {breed= CockerDoodle},
-    {name= Barkley},];
-
-function add (breed, name){
-    return breed + name;
+//11. Fur Babies
+class roomMatesPets 
+{ 
+    name = Barkley;
+    breed = Cockerdoodle;
+}
+        console.log(name + breed)
+{
+    name = Tamale;
+    breed = BlackCat;
+        
+        console.log(name + "is a" + breed)
 }
 
-//Look at all the people
-
-i=name
-var person=[ 
-{name:'Eric', age: 21},
-{name:'Kevin', age: 37},
-{name:"Henry", age: 18},
-{name:"Sam", age: 11}  
+//12.
+i = name
+var friends = [
+    { name: 'Wonho', age: 21 },
+    { name: 'Shownu', age: 27 },
+    { name: "Jooheon", age: 18 },
+    { name: "Kihyun", age: 17 },
+    { name: "Minhyuk", age: 22 },
 ];
 
-function checkAge([age, name]){
-    if (age > 21){
+for (let i = 0; i < friends.length; i++) {
+    checkAge(friends[i].name, friends[i].age)
+}
+
+//step 14: getLength function
+function getLength(string) {
+    return string.length;
+}
+let length = getLength("Hello World");
+
+if (length % 2 === 0) {
+    console.log("The world is nice and even!");
+} else {
+    console.log("The world is an odd place!");
+}
